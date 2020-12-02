458142211b09ce13072a8fd20bab5332918c92d9
Grzesiek <g.chojnacki0@gmail.com>
2020-12-02 11:34:52 +0100

    First commit, Account class

---
+++ src/main/java/Account.java
@@ -0,0 +1,18 @@
+public class Account {
+
+    private boolean active;
+
+    public Account()
+    {
+        this.active=false;
+    }
+
+    public void activate()
+    {
+        this.active=true;
+    }
+
+    public boolean isActive() {
+        return this.active;
+    }
+}
